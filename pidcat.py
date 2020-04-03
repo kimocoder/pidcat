9930fb55abb965b433ff14380a5386949d23f7ca
Sammy Patenotte <sammy.patenotte@yahoo.com>
2020-04-03 10:54:33 -0700

    Fixed package filtering From https://github.com/CertainlyAria/pidcat/commit/09464eded5768e85590f374e1d370892d6c07cb7

--- pidcat.py
+++ pidcat.py
@@ -73,7 +73,7 @@ if len(package) == 0:
   args.all = True
 
 # Store the names of packages for which to match all processes.
-catchall_package = filter(lambda package: package.find(":") == -1, package)
+catchall_package = list(filter(lambda package: package.find(":") == -1, package))
 # Store the name of processes to match exactly.
 named_processes = filter(lambda package: package.find(":") != -1, package)
 # Convert default process names from <package>: (cli notation) to <package> (android notation) in the exact names match group.
